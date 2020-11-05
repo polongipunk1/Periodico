6de611c9a37c2858b9b2993c464d0f9f6539bfa8
Jerry Anthony Gabriel Jacuinde <bandoler71@gmail.com>
2020-11-04 19:09:45 -0600

    Periodico Segunda Parte

--- Practicas/App_Start/BundleConfig.cs
+++ Practicas/App_Start/BundleConfig.cs
@@ -23,7 +23,9 @@ namespace Practicas
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css"));
+                      "~/Content/bootstrap.css",
+                      "~/Content/estilos.css",
+                      "~/Content/global.css"));
         }
     }
 }
