4d510dd8369ac993c8ad1b9e011d115c726ddce1
Jerry Anthony Gabriel Jacuinde <bandoler71@gmail.com>
2020-12-02 19:59:59 -0600

    PlataformasWeb con Bootstrap

--- Practicas/App_Start/BundleConfig.cs
+++ Practicas/App_Start/BundleConfig.cs
@@ -23,9 +23,7 @@ namespace Practicas
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/estilos.css",
-                      "~/Content/global.css"));
+                      "~/Content/bootstrap.css"));
         }
     }
 }
